079945ddacd806360b2d6c4307ff203410a2dd94
Erik Schumacher <erik.d.schumacher@gmail.com>
2023-10-28 23:57:45 -0700

    Add valgrind, memcheck, and cppcheck to cmake project

--- cmake/AddCoverage.cmake
+++ cmake/AddCoverage.cmake
@@ -1,18 +1,13 @@
-# TODO: add this if hitting segfaults for coverage?
-#add_custom_command(TARGET sut PRE_BUILD COMMAND
-#                 find ${CMAKE_BINARY_DIR} -type f
-#                 -name '*.gcda' -exec rm {} +)
-
 function(AddCoverage target)
     find_program(LCOV_PATH lcov REQUIRED)
     find_program(GENHTML_PATH genhtml REQUIRED)
-    add_custom_target(coverage
+    add_custom_target(coverage-${target}
         COMMENT "Running coverage for ${target}..."
         COMMAND ${LCOV_PATH} --gcov-tool ${CMAKE_SOURCE_DIR}/cmake/gcov-llvm-wrapper.sh -d . --zerocounters
         COMMAND $<TARGET_FILE:${target}>
         COMMAND ${LCOV_PATH} --gcov-tool ${CMAKE_SOURCE_DIR}/cmake/gcov-llvm-wrapper.sh -d . --capture -o coverage.info
         COMMAND ${LCOV_PATH} --gcov-tool ${CMAKE_SOURCE_DIR}/cmake/gcov-llvm-wrapper.sh -r coverage.info '/usr/include/*' -o filtered.info
-        COMMAND ${GENHTML_PATH} -o coverage filtered.info --legend
+        COMMAND ${GENHTML_PATH} -o coverage-${target} filtered.info --legend
         COMMAND rm -rf coverage.info filtered.info
         WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
     )
