de815219cc28cb5678ffc114587ffc45939d6803
Erik Schumacher <erik.d.schumacher@gmail.com>
2023-10-29 00:20:28 -0700

    Fixed valgrind memcheck cmake file; deleted superfluous valgrind cmake file

--- cmake/AddMemCheck.cmake
+++ cmake/AddMemCheck.cmake
@@ -1,20 +1,22 @@
-function(AddMemCheck target)
-  include(FetchContent)
-  FetchContent_Declare(
-    memcheck-cover
-    GIT_REPOSITORY https://github.com/Farigh/memcheck-cover.git
-    GIT_TAG        release-1.2
-  )
-  FetchContent_MakeAvailable(memcheck-cover)
-  set(MEMCHECK_PATH ${memcheck-cover_SOURCE_DIR}/bin)
+include(FetchContent)
+FetchContent_Declare(
+  memcheck-cover
+  GIT_REPOSITORY https://github.com/Farigh/memcheck-cover.git
+  GIT_TAG        release-1.2
+)
+FetchContent_MakeAvailable(memcheck-cover)
 
-  add_custom_target(memcheck
+function(AddMemCheck target)
+  set(MEMCHECK_PATH ${memcheck-cover_SOURCE_DIR}/bin)
+  set(REPORT_PATH "${CMAKE_BINARY_DIR}/valgrind-${target}")
+
+  add_custom_target(memcheck-${target}
     COMMAND ${MEMCHECK_PATH}/memcheck_runner.sh -o
-            "${CMAKE_BINARY_DIR}/valgrind/report"
+            "${REPORT_PATH}/report"
             -- $<TARGET_FILE:${target}>
     COMMAND ${MEMCHECK_PATH}/generate_html_report.sh
-            -i "${CMAKE_BINARY_DIR}/valgrind"
-            -o "${CMAKE_BINARY_DIR}/valgrind"
+            -i "${REPORT_PATH}"
+            -o "${REPORT_PATH}"
     WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
   )
 endfunction()
