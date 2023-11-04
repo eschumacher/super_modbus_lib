964116d681d0c3775cc795c7c2be8faed9414b7e
Erik Schumacher <erik.d.schumacher@gmail.com>
2023-11-04 12:53:22 -0700

    [testing] Added dword endian tests

--- test/endian_helpers.cpp
+++ test/endian_helpers.cpp
@@ -17,16 +17,38 @@ TEST(EndianHelpers, WordLE) {
   }
 }
 
+TEST(EndianHelpers, DWordLE) {
+  constexpr uint32_t kVal{0x12345678};
+  constexpr uint32_t kSwapped{0x78563412};
+  auto result = supermodbus::le_swap(kVal);
+
+  if constexpr (std::endian::native == std::endian::big) {
+    EXPECT_EQ(result, kSwapped);
+  } else {
+    EXPECT_EQ(result, kVal);
+  }
+}
+
 TEST(EndianHelpers, WordBE) {
   constexpr uint16_t kVal{0x1234};
   constexpr uint16_t kSwapped{0x3412};
   auto result = supermodbus::be_swap(kVal);
 
   if constexpr (std::endian::native == std::endian::big) {
-    std::cout << "big\n";
     EXPECT_EQ(result, kVal);
   } else {
-    std::cout << "little\n";
+    EXPECT_EQ(result, kSwapped);
+  }
+}
+
+TEST(EndianHelpers, DWordBE) {
+  constexpr uint32_t kVal{0x12345678};
+  constexpr uint32_t kSwapped{0x78563412};
+  auto result = supermodbus::be_swap(kVal);
+
+  if constexpr (std::endian::native == std::endian::big) {
+    EXPECT_EQ(result, kVal);
+  } else {
     EXPECT_EQ(result, kSwapped);
   }
 }
