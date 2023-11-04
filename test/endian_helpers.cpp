bce1c8ff4b5ac267366a6e151d59e7a8be4f41a7
Erik Schumacher <erik.d.schumacher@gmail.com>
2023-11-04 13:02:10 -0700

    [testing] added qword endian tests

--- test/endian_helpers.cpp
+++ test/endian_helpers.cpp
@@ -29,6 +29,18 @@ TEST(EndianHelpers, DWordLE) {
   }
 }
 
+TEST(EndianHelpers, QWordLE) {
+  constexpr uint64_t kVal{0x123456789ABCDEF0};
+  constexpr uint64_t kSwapped{0xF0DEBC9A78563412};
+  auto result = supermodbus::le_swap(kVal);
+
+  if constexpr (std::endian::native == std::endian::big) {
+    EXPECT_EQ(result, kSwapped);
+  } else {
+    EXPECT_EQ(result, kVal);
+  }
+}
+
 TEST(EndianHelpers, WordBE) {
   constexpr uint16_t kVal{0x1234};
   constexpr uint16_t kSwapped{0x3412};
@@ -44,6 +56,18 @@ TEST(EndianHelpers, WordBE) {
 TEST(EndianHelpers, DWordBE) {
   constexpr uint32_t kVal{0x12345678};
   constexpr uint32_t kSwapped{0x78563412};
+  auto result = supermodbus::be_swap(kVal);
+
+  if constexpr (std::endian::native == std::endian::big) {
+    EXPECT_EQ(result, kVal);
+  } else {
+    EXPECT_EQ(result, kSwapped);
+  }
+}
+
+TEST(EndianHelpers, QWordBE) {
+  constexpr uint64_t kVal{0x123456789ABCDEF0};
+  constexpr uint64_t kSwapped{0xF0DEBC9A78563412};
   auto result = supermodbus::be_swap(kVal);
 
   if constexpr (std::endian::native == std::endian::big) {
